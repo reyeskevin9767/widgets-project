231e99cb957279de167ff2b90e77f80f160ee310
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-26 17:18:02 -0400

    add useState

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-  
+  // UseState allows state to be access by a function component
+  // state -> setState -> Inital value
+  const [activeIndex, setActiveIndex] = useState(null);
+
   // Map through items
-  const renderedItems = items.map((item) => {
+  const renderedItems = items.map((item, index) => {
+    // Helper Function
+    // Must wrap callback function in arrow function to invoke later
+    const onTitleClick = (index) => {
+      setActiveIndex(index);
+    };
+
     return (
-      <div className={item.title}>
-        <div className="title active">
+      // React.Fragment - group a list of children without
+      // adding extra elements in the DOM
+      <React.Fragment key={item.title}>
+        <div className="title active" onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
         <div className="content active">
           <p>{item.content}</p>
         </div>
-      </div>
+      </React.Fragment>
     );
   });
 
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
