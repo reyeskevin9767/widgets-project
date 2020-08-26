4c76e562dcfff833582fe73ca63caf999a6c3447
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-26 16:53:57 -0400

    add semantic and styling

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
-  return <h1>{items.length}</h1>;
+  
+  // Map through items
+  const renderedItems = items.map((item) => {
+    return (
+      <div className={item.title}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </div>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
